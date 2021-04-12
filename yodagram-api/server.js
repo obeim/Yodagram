ebd3b59fea0cab368aba7f78e0da941791a5f9af
obei <obei.m2017@gmail.com>
2021-04-12 12:13:11 +0200

    Search Route added

--- yodagram-api/server.js
+++ yodagram-api/server.js
@@ -17,6 +17,11 @@ app.use(express.static(path.join(__dirname, "/uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Yodagram api is running <h1/> <h2>Routes :</h2><h3>/api/users :</h3><h3>/api/upload :</h3><h3>/api/posts :</h3>"
+  );
+});
 app.use("/api/upload", uploadRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/posts", postRoute);
@@ -24,6 +29,6 @@ app.use("/api/posts", postRoute);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`yodagram api runnig on port ${port} `);
 });
