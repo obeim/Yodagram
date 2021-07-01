49b34b4073174393c06b4354d52b39b08d3769fe
obei <obei.m2017@gmail.com>
2021-07-01 14:40:11 +0200

    deploy heroku

--- yodagram-api/middlewares/imageClearingMiddleware.js
+++ yodagram-api/middlewares/imageClearingMiddleware.js
@@ -1,4 +1,4 @@
-import fs from "fs";
+import fs, { existsSync } from "fs";
 import path from "path";
 const __dirname = path.resolve();
 import User from "../models/User.js";
@@ -6,21 +6,25 @@ import Post from "../models/Post.js";
 
 export const imageClearingMiddleware = (req, res, next) => {
 
-  fs.readdir(path.join(__dirname, "/uploads"), (err, files) => {
-    if (files && files.length > 0) {
-      files.forEach(async (file) => {
-        let filePath = "/uploads" + `/${file}`;
-        const postImage = await Post.find({
-          image: filePath,
+  if(existsSync(path.join(__dirname, "/uploads"))){
+    fs.readdir(path.join(__dirname, "/uploads"), (err, files) => {
+      if (files.length > 0) {
+        files.forEach(async (file) => {
+          let filePath = "/uploads" + `/${file}`;
+          const postImage = await Post.find({
+            image: filePath,
+          });
+          const userImage = await User.find({
+            profilePic: filePath,
+          });
+          if (postImage.length === 0 && userImage.length === 0) {
+            fs.unlinkSync(path.join(__dirname, filePath));
+          }
         });
-        const userImage = await User.find({
-          profilePic: filePath,
-        });
-        if (postImage.length === 0 && userImage.length === 0) {
-          fs.unlinkSync(path.join(__dirname, filePath));
-        }
-      });
-    }
-  });
+      }
+    });
+
+  }
+ 
   next();
 };
